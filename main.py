c0c55088d1c6d057dc3d11583335539ce12c09fd
sebdackus <sebdackus@gmail.com>
2025-01-10 14:57:41 +0100

    calculating score

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-
-
-def create_connections(data):
-
+#from load_data import get_possible_directions
+#from verbinding_class import Connection
+#from verbinding_class import Trajectory
+#from code.algorithms import random_start_random_choice #choose_random_connections
+def calculate_score(connections, trajectory_amount, duration, total_connections = 28):
     """
-    reads a csv file and creates Connection objects for every connection listed
-    in the file. Returns a dictionary with the name of the connection as the key,
-    and the corresponding object as the value.
+    Calculates the quality of the itinerary. Outputs a score.
     """
-    connections_dictionary = {}
+    p = connections / total_connections
+    return p * 10000 - (trajectory_amount * 100 + duration)
 
-    # find names of start station, end station and duration of connection
-    for index, row in data.iterrows():
-        station_1 = row.iloc[0]
-        station_2 = row.iloc[1]
-        duration = row.iloc[2]
+if __name__ == "__main__":
 
-        # initialize Connection object with correct data and add to dictionary
-        connections_dictionary[station_1 + "-" + station_2] = Connection(station_1, station_2, duration)
 
-    return connections_dictionary
+
+
+    # dataframe = create_trajectories(3, choose_random_connections, test_dict, original_dict, test_dict, possible_connections)
+    # directions, corrected_df = get_possible_directions("ConnectiesHolland.csv")
+    # test_dict = create_connections(corrected_df)
+    # test_df = create_trajectories(7, random_connection)

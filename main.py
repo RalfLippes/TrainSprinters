5474c0b421cb74fd28689470ea34f17fcd40fd66
RalfLippes <r.lippespam@gmail.com>
2025-01-13 12:36:25 +0100

    corrected mian
    corrected main

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ import copy
 
 def create_connections(data):
     """
-    Calculates the quality of the itinerary. Outputs a score.
+    reads a csv file and creates Connection objects for every connection listed
+    in the file. Returns a dictionary with the name of the connection as the key,
+    and the corresponding object as the value.
     """
-    p = connections / total_connections
-    return p * 10000 - (trajectory_amount * 100 + duration)
+    connections_dictionary = {}
 
-if __name__ == "__main__":
+    # find names of start station, end station and duration of connection
+    for index, row in data.iterrows():
+        station_1 = row.iloc[0]
+        station_2 = row.iloc[1]
+        duration = row.iloc[2]
 
+        # initialize Connection object with correct data and add to dictionary
+        connections_dictionary[station_1 + "-" + station_2] = Connection(station_1, station_2, duration)
 
     return connections_dictionary
 
